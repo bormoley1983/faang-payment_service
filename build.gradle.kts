05cbb46bc6d5800f1de62a336149bf5a089bd425
Bormoley1983 <zhuravel_leonid@yahoo.com>
2026-02-14 17:29:09 +0200

    Actuator added, Java25 updates, config updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ version = "1.0"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
@@ -23,6 +23,7 @@ dependencies {
      * Spring boot starters
      */
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-cache")
